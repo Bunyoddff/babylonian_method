d4a79ba54e901ab9d4ac8c3b355526ab562e50c1
Bunyoddff <bkhusanov444@gmail.com>
2022-12-24 11:44:19 +0500

    bpy

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 def main(S, d):
+
+    a=(S-d**2)/2*d
+    b=a+d
+    x=b-a*a/2*b
     '''create a babylonian function.
     
     Args:
         S (int): number
         d (int): numnber
         
+        
     Returns:
         float: result
     '''
-    return
+    return float(x)
+S=16
+d=4
+print(main(16,4))
